0908872eeeca1c71ef1de3a1429cf29571bd088c
Jannis <jannis.berhausen@cerebricks.com>
2025-05-23 16:18:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 import requests 
 import json
 from azure.core.credentials import AzureKeyCredential
-from azure.communication.callautomation import (CallAutomationClient)
-import os
+from azure.communication.callautomation import (CallAutomationClient, FileSource)
+import os, time
 app = Flask(__name__)
 @app.route("/")
 def home():
@@ -59,12 +59,32 @@ def eventhandler(data, event_type):
             from_details = data.get("from", {}).get("phoneNumber", {}).get("value", "Unbekannt")
             correlation_id = data.get("correlationId", "Unbekannt")
             call_connection_properties = client.answer_call(incoming_call_context, callback_url)
+            # using call connection id, get call connection
+            call_connection = client.get_call_connection(call_connection_properties.call_connection_id)
+            
+            # from callconnection of result above, play media to all participants
+            my_file = FileSource(url="https://github.com/microsoft/call-center-ai/blob/main/public/loading.wav")
+            try:
+                call_connection.play_to_all(my_file)
+            except Exception as e:
+                print(e)
+            try:
+                call_connection.play_media_to_all(my_file)
+            except Exception as e:
+                print(e)
+            time.sleep(3)
+            call_connection.hang_up
+            #https://github.com/microsoft/call-center-ai/blob/main/public/loading.wav
+            
             print(call_connection_properties)
+            return jsonify({'response': '200'})
         case 'Microsoft.EventGrid.SubscriptionValidationEvent':
             validation_code = payload['validationCode']
             return jsonify({'validationResponse': validation_code})
+        case 'Microsoft.Communication.CallDisconnected':
+            return jsonify({'response': '500', 'event_type': event_type })
         case _:
-            return
+            return jsonify({'response': '500','event_type': event_type})
 
 
 if __name__ == "__main__":
